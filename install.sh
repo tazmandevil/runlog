90b3848607b1865b09b199aee5861988966572cb
Tazman DeVille <tazmandevil@gmx.com>
2015-01-17 15:36:44 +0100

    fixed install.sh, edited readme

--- install.sh
+++ install.sh
@@ -7,9 +7,9 @@
 # released according to the terms of the GNU GPL v.3 or later#
 ##############################################################
 
-if [ != "$HOME/bin/" ]; then
+if [ ! -d "$HOME/bin/" ]; then
 	mkdir $HOME/bin/
-	$PATH=$PATH:/$HOME/bin/
+	PATH=$PATH:/$HOME/bin/
 	export PATH
 fi
 
